22d517f4d58910e5055e38be7ac759d5ea0f0ba4
Pratham Gupta <pratham8953771016@gmail.com>
2026-06-11 20:07:33 +0530

    "Removed parsing of Temperatures"

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::{
     collections::{BTreeMap, HashMap},
     fs::File,
-    io::{BufRead, BufReader, Write},
+    io::Write,
     time::Instant,
 };
+
+use memmap2::Mmap;
+
 //
 fn main() {
     let start = Instant::now();
@@ -11,36 +14,68 @@ fn main() {
     let file_size: u64 = std::fs::metadata(path).unwrap().len();
 
     let f: File = File::open(path).unwrap();
-    let f: BufReader<File> = BufReader::new(f);
+    // let f: BufReader<File> = BufReader::new(f);
+    let map: Mmap = mmap(&f);
 
-    let mut stats = HashMap::<Vec<u8>, (f64, f64, usize, f64)>::new();
+    let mut stats = HashMap::<Vec<u8>, (i32, i64, usize, i32)>::new();
     let mut rows = 0usize;
 
-    for line in f.split(b'\n') {
-        let line = line.unwrap();
+    for line in map.split(|c| *c == b'\n') {
+        //  let line = line.unwrap();
+        if line.is_empty() {
+            continue;
+        }
         rows += 1;
 
         let mut fields = line.rsplitn(2, |c| *c == b';');
-
-        let temperature = fields.next().unwrap();
-        let station = fields.next().unwrap();
+        let (Some(temperature), Some(station)) = (fields.next(), fields.next()) else {
+            panic!("bad line: {}", unsafe {
+                std::str::from_utf8_unchecked(line)
+            });
+        };
 
         // SAFETY: the README promised
-        let temperature: f64 = unsafe { std::str::from_utf8_unchecked(temperature) }
-            .parse()
-            .unwrap();
+        // let mut t: i32 = 0;
+        // let mut mul: i32 = 1;
+
+        let mut t: i32 = 0;
+        let mut mul: i32 = 1;
+
+        for &d in temperature.iter().rev() {
+            match d {
+                b'.' => {}
+                b'-' => {
+                    t = -t;
+                    break;
+                }
+                b'0'..=b'9' => {
+                    t += i32::from(d - b'0') * mul;
+                    mul *= 10;
+                }
+                _ => panic!("bad temperature"),
+            }
+        }
+        // let temperature = fields.next().unwrap();
+        // let station = fields.next().unwrap();
+
+        // let temperature: f64 = unsafe { std::str::from_utf8_unchecked(temperature) }
+        //     .parse()
+        //     .unwrap();
 
         let stats = match stats.get_mut(station) {
             Some(stats) => stats,
             None => stats
                 .entry(station.to_vec())
-                .or_insert((f64::MAX, 0., 0, f64::MIN)),
+                .or_insert((i32::MAX, 0, 0, i32::MIN)),
         };
-
-        stats.0 = stats.0.min(temperature);
-        stats.1 += temperature;
+        stats.0 = stats.0.min(t);
+        stats.1 += t as i64;
         stats.2 += 1;
-        stats.3 = stats.3.max(temperature);
+        stats.3 = stats.3.max(t);
+        // stats.0 = stats.0.min(t);
+        // stats.1 += temperature;
+        // stats.2 += 1;
+        // stats.3 = stats.3.max(temperature);
     }
 
     let station_count = stats.len();
@@ -56,7 +91,12 @@ fn main() {
     let mut stats = stats.into_iter().peekable();
 
     while let Some((station, (min, sum, count, max))) = stats.next() {
-        print!("{station}={min:.1}/{:.1}/{max:.1}", sum / (count as f64));
+        print!(
+            "{station}={:.1}/{:.1}/{:.1}",
+            min as f64 / 10.0,
+            sum as f64 / count as f64 / 10.0,
+            max as f64 / 10.0
+        );
 
         if stats.peek().is_some() {
             print!(", ");
@@ -80,8 +120,11 @@ fn main() {
     eprintln!("File size: {:.2} GB", gb);
     eprintln!("Rows processed: {}", rows);
     eprintln!("Stations found: {}", station_count);
-    eprintln!("Execution time: {:.3?}", elapsed);
+    eprintln!("----Execution time----: {:.3?}", elapsed);
     eprintln!("Rows/sec: {:.2} million", rows_per_sec / 1_000_000.0);
     eprintln!("Throughput: {:.2} GB/sec", gb_per_sec);
     eprintln!("===============================");
+}
+fn mmap(f: &File) -> Mmap {
+    unsafe { Mmap::map(f).unwrap() }
 }
